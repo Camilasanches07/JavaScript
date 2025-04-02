f74e00be083e517b350c83f1762d9a0326170981
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-04-02 18:40:15 -0300

    loops

---
+++ loops.js
@@ -0,0 +1,30 @@
+/*let numero = 100*/
+/*while (numero >=1) {
+    if(numero %2 == 0){
+        console.log(numero)
+    }
+    numero--
+}*/
+
+/*let letras = ["C" ,"A", "M", "I", "L"]
+let index = 0
+
+while (index < letras.length/2) {
+    console.log(letras[index])
+    index++
+}
+
+
+do { 
+    console.log(letras[index])
+    index++
+    
+} while (index < letras.length/2);*/
+
+let nome = "Camila"
+let index = nome.length-1
+
+while (index >= 0) {
+    console.log(nome[index])
+    index--
+}
--- testes.js
+++
@@ -1,9 +0,0 @@
-frutas = ["maça", "pêra", "uva", "laranja", "morango"]
-
-frutas.push("goiaba")
-
-frutas.shift()
-
-frutas[2] = "kiwi"
-
-console.log(frutas)
