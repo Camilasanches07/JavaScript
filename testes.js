93753e5ffb80ecad34598d7e390431e3b4f10c69
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-04-07 22:02:53 -0300

    Exercício numeros maior que 100

--- testes.js
+++ testes.js
@@ -1,28 +1,9 @@
-const nomes = ["Pera" , "Uva" , "Banana" , "Kiwi" , "Morango"]
-let index = 4
+const numeros = [100, 40, 33, 670, 5, 4, 880, 900]; //Array com os números
+let index = 0; //Valor/contador do index , ex: [0] = 100, [1] = 40 ... 
 
-switch (nomes [index]) {
-    case "Pera":
-        console.log("Pera - R$2,00 0 kilo")  
-        break;
-
-    case "Uva":
-        console.log("Uva - R$4,00 o kilo")
-        break;
-
-    case "Banana":
-        console.log("Banana - R$3,50 0 kilo")  
-        break;
-
-    case "Kiwi":
-        console.log("Kiwi - R$5,50 0 kilo")  
-        break;
-
-    case "Morango":
-        console.log("Morango - R$2,50 0 kilo")  
-        break;
-
-    default:
-        console.log("Opção indisponível")
-        break;
+while (index < numeros.length) { //Enquanto o index for menor que o tamanho do array
+    if (numeros[index] > 100) { //Verifica se o número atual é maior/igual que 100 , ex: [0] = 100 - verdadeiro, [1] = 40 - falso ...
+        console.log(numeros[index]); //Printa na tela as condições em que os números forem > 100
+    }
+    index++; //Faz ir para a próxima posição do Array
 }
