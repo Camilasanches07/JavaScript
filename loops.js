547a9ecfb113409e5a157ef63bfa918a2551a2f3
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-04-09 18:32:44 -0300

    comentario

--- loops.js
+++ loops.js
@@ -15,16 +15,60 @@ while (index < letras.length/2) {
 }
 
 
-do { 
+do { //Usa-se o "do" para algo que vai ser executado pelo menos uma vez 
     console.log(letras[index])
     index++
     
 } while (index < letras.length/2);*/
 
-let nome = "Camila" //Variávl nome
+/*let nome = "Camila" //Variávl nome
 let index = nome.length-1 //Variável index atrubuida com a variável nome + seu tamanho 
 
 while (index >= 0) { //Enquanto o index for maior ou igual a zero
     console.log(nome[index]) //Printar o nome com a variável index 
     index-- // Subtrair os dados do index
+}*/
+
+/*let letras = ["C" ,"A", "M", "I", "L"]
+
+for (let index = letras.length-1; index >= letras.length/2 ; index--) {
+    const element = letras[index];
+    console.log(element)
+}*/
+
+const pedidos = [
+    {
+        price: 15,
+        id: 1,
+        user:"João",
+        produtos: ["Tênis", "Camisas", "Boné", "Chinelo"]
+    },
+    {
+        price: 30,
+        id:2,
+        name: "Meia",
+        user:"Pedro",
+        produtos: ["Chapéu", "Relógio" , "Pulseira"]
+    },
+    {
+        price: 40,
+        id:3,
+        name: "Calça",
+        user:"Rafael",
+        produtos: ["Brinco" ,"Short"]
+    }
+]
+
+let total = 0
+
+for (let index = 0; index < pedidos.length; index++) { //let index = 0 -> Começa do 1 item do array ,  index < pedidos.length-> continua enquanto o índice for menor que o tamanho do array. index++ -> aumenta 1 a cada volta, para ir para o próximo item.
+    const element = pedidos[index];//Está pegando o item atual do array pedidos e armazenando na constante element.
+    //pedidos[index].price = pedidos[index].price * 0.1 + pedidos[index].price
+    total = pedidos[index].price + total
+    console.log(element.produtos)
+
+    /*if (element.price < 40) {
+        console.log(element.price)
+    }*/
 }
+console.log(total)
--- testes.js
+++
@@ -1,9 +0,0 @@
-const numeros = [100, 40, 33, 670, 5, 4, 880, 900]; //Array com os números
-let index = 0; //Valor/contador do index , ex: [0] = 100, [1] = 40 ... 
-
-while (index < numeros.length) { //Enquanto o index for menor que o tamanho do array
-    if (numeros[index] > 100) { //Verifica se o número atual é maior/igual que 100 , ex: [0] = 100 - verdadeiro, [1] = 40 - falso ...
-        console.log(numeros[index]); //Printa na tela as condições em que os números forem > 100
-    }
-    index++; //Faz ir para a próxima posição do Array
-}
