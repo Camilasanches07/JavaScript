8061bb57412c540a9a2edd0ee876cc773c9519e8
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-04-15 17:56:47 -0300

    matematica

--- testes.js
+++ testes.js
@@ -1,4 +1,19 @@
-function saudacao(nome){
-    console.log("Ola", nome)
+function soma(num1 , num2){
+    console.log(num1 + num2)
 }
-saudacao("Ana")
+soma(10, 50000000)
+
+function subtrai(num1 , num2){
+    console.log(num1 - num2)
+}
+subtrai(100, 50)
+
+function multiplica(num1 , num2){
+    console.log(num1 * num2)
+}
+multiplica(10, 50000000)
+
+function divide(num1 , num2){
+    console.log(num1 / num2)
+}
+divide(10, 2)
