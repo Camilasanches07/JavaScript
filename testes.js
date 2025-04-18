2547b8cef2d0f5222ca16ce89d0cffa487ea1349
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-04-18 18:44:37 -0300

    read

---
+++ testes.js
@@ -0,0 +1,14 @@
+// =============== PARTE 1: ARRAY DE OBJETOS E FUNÇÕES BÁSICAS ===============
+
+let usuarios = [
+    { id: 1, nome: "Ana Silva", email: "ana@email.com", idade: 28 },
+    { id: 2, nome: "Carlos Oliveira", email: "carlos@email.com", idade: 35 },
+    { id: 3, nome: "Mariana Santos", email: "mariana@email.com", idade: 22 }
+  ];
+  
+  // EXERCÍCIO 1: Função para listar todos os usuários (READ)
+  function listarUsuarios() {
+    return usuarios
+  }
+
+  console.log(usuarios)
