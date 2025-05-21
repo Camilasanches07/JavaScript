92857697b4518437f09c0ca0aabf7c8ff59c060c
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-05-21 20:32:00 -0300

    aula react

--- testes.js
+++ testes.js
@@ -1,14 +1,50 @@
-// =============== PARTE 1: ARRAY DE OBJETOS E FUNÇÕES BÁSICAS ===============
+//console.log("Início");
 
-let usuarios = [
-    { id: 1, nome: "Ana Silva", email: "ana@email.com", idade: 28 },
-    { id: 2, nome: "Carlos Oliveira", email: "carlos@email.com", idade: 35 },
-    { id: 3, nome: "Mariana Santos", email: "mariana@email.com", idade: 22 }
-  ];
-  
-  // EXERCÍCIO 1: Função para listar todos os usuários (READ)
-  function listarUsuarios() {
-    return usuarios
-  }
+function getUsers() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = false //ndom() > 0.3; // 70% de chance de sucesso
+            if (success) {
+                resolve(["Alice", "Bob", "Charlie"]);
+            } else {
+                reject("Não foi possível obter a lista de usuários.");
+            }
+        }, 1000);
+    });
+}
 
-  console.log(usuarios)
+/*getUsers().then(users => {
+    console.log("Usuários:", users)
+    renderizaTabela(users)
+}).catch(error => {
+  console.error("Erro:", error)
+  console.log("Não foi possível obter a lista de usuários.")
+});
+
+console.log("ok")*/
+
+function renderizaTabela(arrUsers){
+    console.log(arrUsers)
+}
+
+async function getData(){
+    try {
+        const result = await getUsers()
+        console.log(result)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getData()
+
+/*setTimeout(() => {
+    console.log("Timeout - Executado após 0ms");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("Promise resolvida - Microtask");
+});
+
+console.log("Fim");*/
