9672b708c163a5bac8fa22aff225e09ce27c3a6b
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-05-28 20:36:26 -0300

    forEach, map, filter, reduce

--- testes.js
+++ testes.js
@@ -1,26 +1,68 @@
-const produtosEletrodomesticos = [
-  { id: 1, nome: "Geladeira Frost Free", valor: 3200.00, temDesconto: true },
-  { id: 2, nome: "Máquina de Lavar 12kg", valor: 2100.50, temDesconto: false },
-  { id: 3, nome: "Fogão 4 Bocas", valor: 899.99, temDesconto: true },
-  { id: 4, nome: "Micro-ondas 30L", valor: 680.00, temDesconto: false },
-  { id: 5, nome: "Aspirador de Pó", valor: 450.30, temDesconto: true },
-  { id: 6, nome: "Ar-condicionado 9000 BTUs", valor: 1899.90, temDesconto: false },
-  { id: 7, nome: "Liquidificador", valor: 199.90, temDesconto: true },
-  { id: 8, nome: "Panela Elétrica", valor: 349.00, temDesconto: false },
-  { id: 9, nome: "Fritadeira sem Óleo", valor: 499.99, temDesconto: true },
-  { id: 10, nome: "Cafeteira Elétrica", valor: 289.90, temDesconto: false }
-];
 
-const novosProdutos = produtosEletrodomesticos.map((produto) => {
-  const novoPreco = produto.temDesconto ? produto.valor * 0.1 : produto.valor
-  
-  return{
-    id: produto.id,
-    nome: produto.nome,
-    valor: novoPreco,
-    temDesconto: produto.temDesconto
-  }
-  
+const produtos = [{
+    name: "Relógio",
+    price:10
+},
+{
+    name:"Short",
+    price:20
+},
+{
+    name:"Vestido",
+    price:55
+},
+{
+    name:"óculos",
+    price:70
+}
+]
+let total = 0
+
+/*for (let index = 0; index < produtos.length; index++) {
+    const element = produtos[index]
+    total = produtos[index].price + total
+
+}*/
+
+/*produtos.forEach((p) => { //Imprimir do primeiro ao último item da lista. Alterar valor de variaveis, etc
+  total = p.price + total
 })
 
-console.log(novosProdutos)
+console.log(total) 
+
+
+
+const produtosFiltrados = produtos.filter((p) => { //Retorna um array. Se retornar "true" retorna o array com os itens filtrados
+  if (p.price > 20) {
+    return true
+  }
+})
+
+console.log(produtosFiltrados) 
+
+
+const produtosAtualizados = produtos.map((p) => { //Percorrer a lista e fazer alterações
+  p.price =  p.price + p.price * 0.1
+  return p
+})
+
+console.log(produtosAtualizados) */
+
+
+ /*const produtosAtualizados = produtos.reduce((acumulador, valorAtual) =>{ //Percorrer a lista e reduzir, ex:somar todos os valores e chegar a um valor finl
+  return acumulador * valorAtual.price
+ }, 1)
+
+ console.log(produtosAtualizados) */
+ let indexEncontrado = null
+
+ for (let index = 0; index < produtos.length; index++) {
+  const element = produtos[index];
+  
+  if (element.name == "Vestido") { 
+    indexEncontrado = index
+  }
+  console.log(element)
+  
+ }
+ console.log(indexEncontrado)
