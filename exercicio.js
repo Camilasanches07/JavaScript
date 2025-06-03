86955630a6023e282052b6976ab07260031b607f
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-06-03 18:05:12 -0300

    reduce

--- exercicio.js
+++ exercicio.js
@@ -1,15 +1,9 @@
-//Use filter para criar um novo array apenas com pessoas maiores de idade (18+).
+//1. Dado o array [5, 10, 15, 20], use reduce para calcular a soma de todos os números.
 
-const pessoas = [
-  {nome: 'Ana', idade: 17},
-  {nome: 'Carlos', idade: 25},
-  {nome: 'Maria', idade: 16},
-  {nome: 'João', idade: 30}
-];
+const numeros = [5, 10, 15, 20]
 
+const soma = numeros.reduce((acumulador, numero) => {
+    return(acumulador + numero)
+}, 0)
 
-const maior_de_idade = pessoas.filter((pessoa) => {
-    return pessoa.idade >= 18
-})
-
-console.log(maior_de_idade)
+console.log(soma)
