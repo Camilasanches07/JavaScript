3ed315d9b930fa4c475b479a97244d80e3568b36
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-06-03 14:09:19 -0300

    .

--- exercicio.js
+++ exercicio.js
@@ -1,9 +1,13 @@
-//2. Dado o array ['javascript', 'python', 'java'], use map para criar um novo array com cada string em maiúscula.
+//5. Dado o array ['João', 'Maria', 'Pedro'], use map para criar um novo array onde cada nome vira "Sr. João", "Sra. Maria", "Sr. Pedro".
 
-const nomes = ['javascript', 'python', 'java']
+const nomes = ['João', 'Maria', 'Pedro']
 
-const maiuscula = nomes.map((n) => {
-    return (n.toUpperCase())
+const novoArray = nomes.map((nome) => {
+    if (nome == "Maria") {
+        return("Sra. " + nome)
+    } else {
+        return("Sr. " + nome)
+    }
 })
 
-console.log(maiuscula)
+console.log(novoArray)
