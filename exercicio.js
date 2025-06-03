0b84db299496a19942532b24ced77c38f4564834
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-06-03 13:45:58 -0300

    verifica se é par

--- exercicio.js
+++ exercicio.js
@@ -1,7 +1,9 @@
-//3. Dado o array [10, 20, 30, 40], use forEach para imprimir o dobro de cada número.
+//4. Dado o array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use forEach para imprimir apenas os números pares.
 
-const numeros = [10, 20, 30, 40] 
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-numeros.forEach((numero) => {
-    console.log(numero * 2)
+numeros.forEach((n) => {
+    if (n % 2 == 0) {
+        console.log(n, "é par!")
+    }
 })
