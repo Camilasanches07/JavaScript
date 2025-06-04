fd3e38d8490f170fe2cd9d572118e361aeb7a161
Camilasanches07 <camilasanchesferreira@hotmail.com>
2025-06-04 13:46:53 -0300

    Simulador de api

--- exercicio.js
+++ exercicio.js
@@ -1,32 +1,27 @@
-//. Convertendo callback para Promise. Transforme esta função que usa callback em uma que retorna Promise:
-/*function lerArquivo(nome, callback) {
-  setTimeout(() => {
-    if (nome === "arquivo.txt") {
-      callback(null, "Conteúdo do arquivo");
-    } else {
-      callback("Arquivo não encontrado", null);
-    }
-  }, 500);
-}*/
+/*4. Simulador de API
+Crie uma função buscarUsuario(id) que:
+Retorna uma Promise
+Espera 2 segundos
+Se id for menor que 10, resolve com {id: id, nome: "Usuario" + id}
+Se id for maior ou igual a 10, rejeita com "Usuário não encontrado"*/
 
-
-
-function lerArquivo(nome){
+function buscarUsuario(id){
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-    if (nome === "arquivo.txt") {
-      resolve("Conteúdo do arquivo");
-    } else {
-      reject("Arquivo não encontrado");
-    }
-  }, 500);
-})
+    setTimeout(() => {
+      if (id < 10) {
+        resolve({id: id, nome: "Usuario" + id})
+        
+      } else {
+        reject("Usuário não encontrado")
+      }
+    }, 2000)
+  })
 }
 
-lerArquivo("arquivo.txt")
-  .then((conteudo) => {
-    console.log("Sucesso:", conteudo)
+buscarUsuario(15)
+  .then((usuario) => {
+    console.log("Usuário Encontrado", usuario )
   })
-  .catch((erro) => {
-    console.log("Erro:", erro)
+  .catch((error) => {
+    console.log("Erro:", error)
   })
